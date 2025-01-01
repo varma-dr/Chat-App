af1214ba52c3b3f4851d2b0fe2deb45cf78d8790
vamra-dr <varma.dr16@gmail.com>
2025-01-01 11:42:17 -0500

    Initial Commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const login = (req, res) => {
+    res.send("login");
+    console.log("login");
+}
+
+export const logout = (req, res) => {
+    res.send("logout");
+    console.log("logout");
+}
+
+export const signup = (req, res) => {
+    res.send("signup");
+    console.log("signup");
+}
