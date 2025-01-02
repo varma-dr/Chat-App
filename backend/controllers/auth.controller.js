7be6342641a6826884ddf7ed02ad8c73c3720e12
vamra-dr <varma.dr16@gmail.com>
2025-01-02 11:59:13 -0500

    Added missing middleware configuration in server.js i.e, app.use(express.json());

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,15 @@
 export const login = (req, res) => {
-    res.send("login");
+    res.send("login user - welcome to new chat app please login and stay connected");
     console.log("login");
 }
 
 export const logout = (req, res) => {
-    res.send("logout");
+    res.send("logout user - thanks for using our chat app, see you soon");
     console.log("logout");
 }
 
 export const signup = (req, res) => {
-    res.send("signup");
+    res.send("signup user - welcome to new chat app please signup and stay connected");
     console.log("signup");
 }
+
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app = express() ;
 dotenv.config();
 const PORT = process.env.PORT || 5002;
 
+app.use(express.json());    
+
 app.get("/", (req, res) => {
     res.send("Hello World - Chat App");
 });
