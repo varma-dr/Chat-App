875aba6d4b0eb687baba1d86ee5a82211912b0fa
vamra-dr <varma.dr16@gmail.com>
2025-01-02 13:20:33 -0500

    MongoDB Connection Started

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,15 @@
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
+    /// TEST START
     res.send("signup user - welcome to new chat app please signup and stay connected");
     console.log("signup");
+    /// TEST END
 }
+    try {
+        const{fullname, username, password, confirmPassword, gender} = req.body;
+    } catch (error) {
+        
+    }
 
 export const login = (req, res) => {
     res.send("login user - welcome to new chat app please login and stay connected");
