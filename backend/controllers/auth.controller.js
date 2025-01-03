4e8ba073a845247e7958b77ef8b380a53a7713e2
vamra-dr <varma.dr16@gmail.com>
2025-01-03 13:01:55 -0500

    New files added to connect MongoDB

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,25 +1,18 @@
 
 export const signup = async (req, res) => {
-    /// TEST START
-    res.send("signup user - welcome to new chat app please signup and stay connected");
-    console.log("signup");
-    /// TEST END
-}
-    try {
-        const{fullname, username, password, confirmPassword, gender} = req.body;
-    } catch (error) {
-        
-    }
+     try {
+        const{ fullname, username, password, confirmPassword, gender} = req.body;
+    } catch (error) {}
+
+};
 
 export const login = (req, res) => {
     res.send("login user - welcome to new chat app please login and stay connected");
     console.log("login");
-}
+};
 
 export const logout = (req, res) => {
     res.send("logout user - thankyou for using our chat app, see you soon");
     console.log("logout");
-}
-
-
+};
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv'
+dotenv.config();
 import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express() ;
 
-dotenv.config();
 const PORT = process.env.PORT || 5002;
 
 app.use(express.json());    
@@ -17,6 +18,7 @@ app.use("/api/auth",authRoutes);
 
 
 app.listen(PORT, () => {
+    connectToMongoDB();
     console.log(`Server is running on port ${PORT}`);
 });
 
