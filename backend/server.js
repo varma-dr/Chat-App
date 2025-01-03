565275a4b98fa2c0cb0be1e22a6638f297a42d37
vamra-dr <varma.dr16@gmail.com>
2025-01-03 14:41:01 -0500

    Developing SignUp Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv'
-dotenv.config();
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
+dotenv.config();
 const app = express() ;
 
 const PORT = process.env.PORT || 5002;
